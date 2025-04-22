ba5a6f18f0b0ace1265f71d2c1af38df89fcac97
nargizichka <117124549+nargizichka@users.noreply.github.com>
2025-04-22 16:08:08 +0500

    Update index.js

--- src/components/icons/index.js
+++ src/components/icons/index.js
@@ -2,7 +2,7 @@ export { default as LogoIconBlack } from "./LogoIconBlack";
 export { default as TelegramIcon } from "./TelegramIcon";
 export { default as InstagramIcon } from "./InstagramIcon";
 export { default as TwitterIcon } from "./TwitterIcon";
-export { default as FaceBookIcon } from "./FaceBookIcon";
+export { default as FaceBookIcon } from "./FacebookIcon";
 export { default as BurgerMenu } from "./BurgerMenu";
 export { default as SearchIcon } from "./SearchIcon";
 export { default as DownArrowIcon } from "./DownArrowIcon";
